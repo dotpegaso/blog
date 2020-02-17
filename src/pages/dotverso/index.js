958e2b7bda6f8935f512e7364b00ad2360c9fbd9
dotpegaso <dotpegaso@gmail.com>
2020-02-17 04:21:58 -0300

    temp removed style.js

--- src/pages/dotverso/index.js
+++ src/pages/dotverso/index.js
@@ -1,5 +1,22 @@
 import React from 'react'
-import { Container, Video } from './styles'
+
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  background-color: #11314C;
+
+  @media screen and (max-width: 490px) {
+    flex-wrap: wrap;
+  }
+`
+
+export const Video = styled.video`
+  width: 50vw;
+  margin: 0 auto;
+  display: block;
+`
 
 const Dotverse = () => (
   <Container>
